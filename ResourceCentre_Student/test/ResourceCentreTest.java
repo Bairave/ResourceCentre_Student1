4ae7f8e365a0979a13a310a79fbd133e2025342e
baira <baira@Bairave>
2022-04-27 11:04:36 +0800

    name change

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 // C206 AY2022 SEM1 - Eclipse 21-09 JDK16
+//bairave
 public class ResourceCentreTest {
 	private Camcorder cc1;
 	private Camcorder cc2;
